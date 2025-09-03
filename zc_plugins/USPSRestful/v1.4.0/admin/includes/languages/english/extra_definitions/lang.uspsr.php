eeab2788e5449a91024ed76b56a58bf3c795577e
Paul Williams <retched@hotmail.com>
2025-09-02 20:01:35 -0400

    Version stamping - 1.4.0

--- zc_plugins/USPSRestful/v1.4.0/admin/includes/languages/english/extra_definitions/lang.uspsr.php
+++ zc_plugins/USPSRestful/v1.4.0/admin/includes/languages/english/extra_definitions/lang.uspsr.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * USPS Shipping (RESTful) for Zen Cart
- * Version 0.0.0
+ * Version 1.4.0
  * 
  * @copyright Portions Copyright 2004-2025 Zen Cart Team
  * @author Paul Williams (retched)
- * @version $Id: lang.uspsr.php 0000-00-00 retched Version 0.0.0 $
+ * @version $Id: lang.uspsr.php 2025-09-02 retched Version 1.4.0 $
 ****************************************************************************
     USPS Shipping (w/REST API) for Zen Cart
     A shipping module for ZenCart, an ecommerce platform
