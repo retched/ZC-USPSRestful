cdd046631470f7a363a00a3744e3b4e8e7bc2969
Paul Williams <retched@hotmail.com>
2026-09-22 16:37:34 -0400

    Update uspsr.php

--- zc_plugins/USPSRestful/v0.0.0/catalog/includes/modules/shipping/uspsr.php
+++ zc_plugins/USPSRestful/v0.0.0/catalog/includes/modules/shipping/uspsr.php
@@ -2272,6 +2272,7 @@ class uspsr extends base
                     }
                 case "v1.8.4": // Released 2026-02-27: No changes, but this is a repair release
                 case "v1.8.5": // Released 2026-07-17: No major database changes.
+                case "v1.8.6": // Released 2026-09-22: No major database changes.
                     break;
             }
 
